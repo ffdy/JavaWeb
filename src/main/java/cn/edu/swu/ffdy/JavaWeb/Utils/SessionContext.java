8fc40fa4c4ebe3c1841bc2ae7c5a1acefe1f0dfb
ffdy <1836017424@qq.com>
2021-12-07 18:04:00 +0800

    添加Session内容工具类

---
+++ src/main/java/cn/edu/swu/ffdy/JavaWeb/Utils/SessionContext.java
@@ -0,0 +1,6 @@
+package cn.edu.swu.ffdy.JavaWeb.Utils;
+
+public class SessionContext {
+    public final static String LOGIN_STATUS = "LOGIN_STATUS";
+    public final static String LOGIN_VALIDATE_CODE = "LOGIN_VALIDATE_CODE";
+}
